a894959be2eefb898ccee8b385973d295423377e
OA <disposuka@gmail.com>
2011-11-01 17:27:17 +0000

    Added html_safe call required in Rails 3

--- lib/app/helpers/form_helper.rb
+++ lib/app/helpers/form_helper.rb
@@ -4,7 +4,8 @@ module FormHelper
   def datepicker(att)
     model = self.object_name
     html = self.text_field att
-    html += '<script type="text/javascript">jQuery(document).ready(function(){$("#'+model+'_'+att.to_s+'").datepicker()});</script>'
+    tag = '<script type="text/javascript">jQuery(document).ready(function(){$("#'+model+'_'+att.to_s+'").datepicker()});</script>'
+    html << tag.html_safe
     return html
   end
   
