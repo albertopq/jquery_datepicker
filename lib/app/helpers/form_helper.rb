ea7a01bcbd7a96c997cb365a32487c27f51042fd
Shawn Stricker <kb1ibt@me.com>
2011-11-02 19:37:42 -0400

    Attempted fix for the nesting attributes

--- lib/app/helpers/form_helper.rb
+++ lib/app/helpers/form_helper.rb
@@ -3,6 +3,8 @@ module FormHelper
   # Mehtod that generates datepicker input field inside a form
   def datepicker(att)
     model = self.object_name
+    model.gsub!(/\]\[|\[/,'_')
+    model.gsub!(/\]/,'')
     html = self.text_field att
     tag = '<script type="text/javascript">jQuery(document).ready(function(){$("#'+model+'_'+att.to_s+'").datepicker()});</script>'
     html << tag.html_safe
