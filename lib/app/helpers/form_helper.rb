3732ab78c194d316bf1e4620254b18e899b12d45
albertopq <albert.pastor@gmail.com>
2011-11-07 09:12:19 +0000

    [BUGFIX] Fixing the bug reported on #6 Issue (https://github.com/albertopq/jquery_datepicker/issues/6)

--- lib/app/helpers/form_helper.rb
+++ lib/app/helpers/form_helper.rb
@@ -8,9 +8,8 @@ module JqueryDatepicker
     def datepicker(object_name, method, options = {})
       input_tag =  JqueryDatepicker::InstanceTag.new(object_name, method, self, options.delete(:object))
       dp_options, tf_options =  input_tag.split_options(options);
-      new_options = input_tag.get_to_input_field_tag_options("text", tf_options)
-      html = input_tag.tag("input", new_options)
-      html += javascript_tag("jQuery(document).ready(function(){$('##{new_options["id"]}').datepicker(#{dp_options.to_json})});")
+      html = input_tag.to_input_field_tag("text", tf_options)
+      html += javascript_tag("jQuery(document).ready(function(){$('##{input_tag.get_name_and_id["id"]}').datepicker(#{dp_options.to_json})});")
       html.html_safe
     end
 
@@ -29,16 +28,7 @@ class JqueryDatepicker::InstanceTag < ActionView::Helpers::InstanceTag
   # Extending ActionView::Helpers::InstanceTag module to make Rails build the name and id
   # Just returns the options before generate the HTML in order to use the same id and name (see to_input_field_tag mehtod)
   
-  def get_to_input_field_tag_options(field_type, options = {})
-    options = options.stringify_keys
-    options["size"] = options["maxlength"] || DEFAULT_FIELD_OPTIONS["size"] unless options.key?("size")
-    options = DEFAULT_FIELD_OPTIONS.merge(options)
-    if field_type == "hidden"
-      options.delete("size")
-    end
-    options["type"]  ||= field_type
-    options["value"] = options.fetch("value"){ value_before_type_cast(object) } unless field_type == "file"
-    options["value"] &&= html_escape(options["value"])
+  def get_name_and_id(options = {})
     add_default_name_and_id(options)
     options
   end
