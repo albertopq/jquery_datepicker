3f879363683ed47ef6bfccc93a5c15cf71b96124
Philip Hale <p.hale.09@aberdeen.ac.uk>
2013-06-28 23:51:47 +0100

    Pass options directly to base#initialise

--- lib/app/helpers/form_helper.rb
+++ lib/app/helpers/form_helper.rb
@@ -6,7 +6,7 @@ module JqueryDatepicker
 
     # Mehtod that generates datepicker input field inside a form
     def datepicker(object_name, method, options = {}, timepicker = false)
-      input_tag =  JqueryDatepicker::InstanceTag.new(object_name, method, self, options.delete(:object))
+      input_tag =  JqueryDatepicker::InstanceTag.new(object_name, method, self, options)
       dp_options, tf_options =  input_tag.split_options(options)
       tf_options[:value] = input_tag.format_date(tf_options[:value], String.new(dp_options[:dateFormat])) if  tf_options[:value] && !tf_options[:value].empty? && dp_options.has_key?(:dateFormat)
       html = input_tag.to_input_field_tag("text", tf_options)
